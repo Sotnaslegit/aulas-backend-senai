ca95e629c28c6dd32090f348225dc6b24c79ec50
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-08-06 20:06:14 -0300

    first commit

---
+++ aula-3/atividades.js
@@ -0,0 +1,66 @@
+let n1 = "150" //para int = 150
+let n2 = 18 // para string = "18"
+let n3 = "20.99" // para float = 20.99
+let n4 = "010" //para int = 10
+
+// function verificaCpf (n) {
+//     if(n.toString().length == 11){
+//         console.log("Bacanudo")
+//     } else {
+//         console.log("Foi não")
+//     }
+// }
+
+// console.log(verificaCpf(12345678910))
+
+// let nome = "taligado"
+// let idade = 10
+// let notaFinal = 10
+// let aprovado= true
+// let aluno={nome,idade,notaFinal,aprovado}
+
+// console.log(aluno)
+
+// let mensagem = `O aluno ${nome}, de ${idade} anos, teve sua média final de ${notaFinal} e a sua aprovação é ${aprovado}`
+
+// console.log(mensagem)
+
+// let calculadora = (a1, a2) => {
+//     let idade = a1 - a2
+//     let mensagem = `Sua idade é ${idade}`
+//     console.log(mensagem)
+// }
+
+// console.log(calculadora(2025, 2000))
+
+// let soraia = 0
+// soraia++
+// soraia = 1
+
+// let bichinho = 5
+// bichinho ++
+// bichinho = 6
+
+// let mateus = 40
+// mateus--
+// mateus = 39
+
+// let numero =10
+// numero += 5
+// numero = 15
+
+// let saldo = 500
+// saldo =- 200
+// saldo = 300
+
+// let b1 = 9
+// b1 *= 2
+// b1 = 18
+
+// let aux = 1
+// aux = aux + 9
+// aux = 10
+
+// let aux = 5
+// aux = aux -5
+// aux = 0
