4673cf50a43718bc7448ac13e35327c8a3a174bb
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-09-11 21:54:51 -0300

    arquivos aulas 18

--- aula-18/src/controllers/taskController.js
+++ aula-18/src/controllers/taskController.js
@@ -15,4 +15,37 @@ const getById = (req, res, next) => {
     };
 };
 
-module.exports = { getAll, getById}
+//create, post e update
+const create = (req, res, next) => {
+    try{
+        const{tittle} = req.body;
+        const task = taskService.create(tittle);
+        res.status(201).json(task);
+    } catch(error){
+        next(error);
+    };
+};
+
+//update
+const update = (req, res, next) => {
+    try{
+        const id = parseInt(req.params.id);
+        const{tittle, done} = req.body;
+        const task = taskService.update(tittle);
+        res.status(200).json(task);
+    } catch(error){
+        next(error)
+    }
+}
+
+const remove = (req, res, next) => {
+    try{
+        const id = parseInt(req.params.id);
+        const deleted = taskService.del(id);
+        res.json({msg: "Tarefinha excluida"});
+    } catch(error){
+        next(error)
+    }
+}
+
+module.exports = { getAll, getById, update, create, remove}
--- aula-18/src/services/taskService.js
+++ aula-18/src/services/taskService.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 let tasks = [
-    {id: 1, title: "Estudar API's", done: false},
-    {id: 2, title: "Praticar Docker", done: true}
+    {id: 1, tittle: "Estudar API's", done: false},
+    {id: 2, tittle: "Praticar Docker", done: true}
 ]
 
 //titulo obrigatorio, n pode duplicar, tarefa só conclui se existe, n pode atualizar ou excluir uma tarefa q n existe e n pode excluir tarefa concluida
@@ -15,4 +15,52 @@ const getById = (id) => {
     return task
 };
 
-module.exports = { getAll, getById}
+const update = (id, {tittle, done}) => {
+    const task = getById(id);
+
+    if(tittle){
+        const exists = task.some(
+            t => t.tittle.toLowerCase() === tittle.toLowerCase && t.id !== id
+        );
+        if(exists)
+            throw {status: 400, message: "Já existe outra tarefa com este titulo"};
+            task.tittle = tittle;
+    };
+
+    if(done !== undefined){
+        if(typeof done !== "boolean")
+            throw {status: 400, message: "O campo deve ser 'true' ou 'false'"}
+        task.done = done;
+    }
+};
+
+const create = (tittle) => {
+    if(!tittle)
+        throw {status: 400, message: "O campo 'tittle' é obrigatório"}
+
+    const exists = tasks.some(
+        t.tittle.toLowerCase() === tittle.toLowerCase()
+    )
+
+    if(exists)
+        throw {status: 400, message: "Já existe uma tarefa com este título"}
+
+    const newTask = {id: tasks.length + 1, tittle, done: false}
+    tasks.push(newTask)
+    return newTask
+};
+
+const del = (id) => {
+    const taskIndex = tasks.findIndex(t => t.id === id)
+    if(taskIndex === -1)
+        throw {status: 404, message: "Tarefa não encontrada"}
+
+    if(tasks[taskIndex].done){
+        throw {status: 400, message: "Não é permitido excluir uma tarefa concluída"}
+    }
+
+    const deleted = tasks.splice(taskIndex, 1)[0]
+    return deleted
+}
+
+module.exports = { getAll, getById, del, create, update}
