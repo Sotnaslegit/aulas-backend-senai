0ff5e927e25da092ea395c85225693d2ec0f01b7
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-09-26 22:07:04 -0300

    fazendo rotas com professor alex

--- professor-alex/aula-1/src/app.js
+++ professor-alex/aula-1/src/app.js
@@ -7,6 +7,7 @@ const pool = await mysql.createConnection({
     database: 'api_node'
 });
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Olá Mundo");
@@ -25,6 +26,51 @@ app.get('/usuarios/:id', async (req, res) => {
         'SELECT * FROM usuario WHERE id=?', id
     );
     res.send(results);
+});
+
+app.post('/usuarios', async (req, res) => {
+    try {
+        const { body } = req
+        const [results] = await pool.query(
+            'INSERT INTO usuario (nome, idade) VALUES (?, ?)',
+            [body.nome,
+            body.idade]
+        );
+
+        const [usuarioCriado] = await pool.query(
+            'SELECT * FROM usuario WHERE id=?', results.insertId
+        );
+
+        return res.status(201).json(usuarioCriado)
+    } catch (error) {
+        console.log(error);
+    };
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const [results] = await pool.query(
+            'DELETE FROM usuario WHERE id=?', id
+        );
+        res.status(200).send("Usuário deletado com sucesso.", results)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { body} = req
+    
+        const [results] = await pool.query(
+            'UPDATE usuario SET `nome` = ?, `idade` = ? WHERE id = ?', [body.nome, body.idade, id]
+        );
+        res.status(200).send('Usuario atualizado', results)
+    } catch (error) {
+        console.log(error)
+    };
 });
 
 const PORT = 3000;
